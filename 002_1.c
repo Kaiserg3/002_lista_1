f3a2f11db57164b14b23d7ba0a79ae91383679cf
Kaiserg3 <133825803+Kaiserg3@users.noreply.github.com>
2023-08-29 09:20:26 -0300

    002_1.c

---
+++ 002_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  int main(){
+    unsigned int x;
+    printf("Insira um número: ");
+      scanf("%i", &x);
+    printf("Valor em Hexa: %x\n",x);
+    printf("Valor em Octal: %o\n",x);
+
+return 0;
+    
+  }
